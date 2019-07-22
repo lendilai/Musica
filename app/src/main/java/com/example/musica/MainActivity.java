e0fc3b934f311f553e87ce0b88685c23fcc884a8
lendilai <ngethenan768@gmail.com>
2019-07-22 03:50:20 +0300

    Create an array adapter for the song details and add the songs info to strings.xml file

--- app/src/main/java/com/example/musica/MainActivity.java
+++ app/src/main/java/com/example/musica/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.musica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.categories) LinearLayout mCategoriesIcon;
+    @BindView(R.id.songs) LinearLayout mSongsIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/musica/SongsAdapter.java
@@ -0,0 +1,35 @@
+package com.example.musica;
+
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+public class SongsAdapter extends ArrayAdapter {
+    private String[] mSongNames;
+    private String[] mArtists;
+    private String[] mDurations;
+    private String[] mAlbums;
+    private Context mContext;
+
+    public SongsAdapter(Context mContext, int resource, String[] names, String[] artists, String[] durations, String[] albums){
+        super(mContext, resource);
+        this.mContext = mContext;
+        this.mSongNames = names;
+        this.mArtists = artists;
+        this.mDurations = durations;
+        this.mAlbums = albums;
+    }
+
+    @Override
+    public Object getItem(int position){
+        String songName = mSongNames[position];
+        String artist = mArtists[position];
+        String duration = mDurations[position];
+        String album = mAlbums[position];
+        return String.format("%s by %s in the album %s \n Duration : %s", songName, artist, album, duration);
+    }
+
+    @Override
+    public int getCount(){
+        return mSongNames.length;
+    }
+}
