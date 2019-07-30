6dccc45b533b1be9178a072e0a9008272c79fc0e
lendilai <ngethenan768@gmail.com>
2019-07-30 12:10:37 +0300

    Create a method to process the results from the api

--- app/src/main/java/com/example/musica/SpotifyService.java
+++ app/src/main/java/com/example/musica/SpotifyService.java
@@ -1,10 +1,25 @@
 package com.example.musica;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.List;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.OkHttpClient;
 import okhttp3.HttpUrl;
 import okhttp3.Request;
+import okhttp3.Response;
 
 
 public class SpotifyService {
@@ -20,5 +35,24 @@ public class SpotifyService {
                 .build();
         Call call = okHttpClient.newCall(request);
         call.enqueue(callback);
+    }
+
+    public static ArrayList<Song> processResults(Response response){
+        ArrayList<Song> foundSongs = new ArrayList<>();
+        Gson gson = new Gson();
+        try {
+            String jsonData = response.body().string();
+            JSONObject deezerJson = new JSONObject(jsonData);
+            JSONArray tracksArray = deezerJson.getJSONArray("track");
+            if (response.isSuccessful()){
+                Type listType = new TypeToken<List<Song>>() {}.getType();
+                foundSongs = gson.fromJson(tracksArray.toString(), listType);
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }catch (JSONException ex){
+            ex.printStackTrace();
+        }
+        return foundSongs;
     }
 }
